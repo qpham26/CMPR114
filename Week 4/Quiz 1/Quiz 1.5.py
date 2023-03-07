b04ec255a40b49abe4def7324de0fdf84766d076
qpham26 <qpham26@gmail.com>
2023-03-06 19:35:45 -0800

    Merge remote-tracking branch 'origin/main'

--- Week 4/Quiz 1/Quiz 1.5.py
+++ Week 4/Quiz 1/Quiz 1.5.py
@@ -1,11 +1,12 @@
-#Program takes a range of sales and shows the commission
+#Program takes a range of sales, and shows the commission
+#and add the commission to the salary
 sales = float(input("Enter your sale: "))
 
-if 50000 <= sales < 70000:
+if 50000 <= sales <= 70000:
     print(f'Your total sale is ${sales:,.2f}')
-    sales = sales * .1
+    salary = 70000.00*
     print(f'Your commission of 10% is ${sales:,.2f}')
-elif 70000 <= sales < 90000:
+elif 70000 <= sales <= 80000:
     print(f'Your total sale is ${sales:,.2f}')
     sales = sales * .2
     print(f'Your commission of 20% is ${sales:,.2f}')
@@ -13,7 +14,6 @@ elif 90000 <= sales:
     print(f'Your total sale is ${sales:,.2f}')
     sales = sales * .3
     print(f'Your commission of 30% is ${sales:,.2f}')
-
 else:
     print(f'Your total sale is ${sales}')
     print("Value of sales if not listed in the provided table")
