dba896125686c162bf599d57cc06a4ed82365e55
qpham26 <qpham26@gmail.com>
2023-03-06 19:20:07 -0800

    Merge remote-tracking branch 'origin/main'

---
+++ Week 4/Quiz 1/Quiz 1.4.py
@@ -0,0 +1,23 @@
+#Program takes a range of sales and shows the commission
+sales = float(input("Enter your sale: "))
+
+if 50000 <= sales <= 60000:
+    print(f'Your total sale is ${sales:,.2f}')
+    sales = sales * .1
+    print(f'Your commission of 10% is ${sales:,.2f}')
+elif 70000 <= sales <= 80000:
+    print(f'Your total sale is ${sales:,.2f}')
+    sales = sales * .2
+    print(f'Your commission of 20% is ${sales:,.2f}')
+elif 90000 <= sales <= 100000:
+    print(f'Your total sale is ${sales:,.2f}')
+    sales = sales * .3
+    print(f'Your commission of 30% is ${sales:,.2f}')
+elif sales > 100000 #Assume 30% commission for sales over 100K
+    print(f'Your total sale is ${sales:,.2f}')
+    sales = sales * .3
+    print(f'Your commission of 30% is ${sales:,.2f}')
+else:
+    print(f'Your total sale is ${sales}')
+    print("You are not eligible for a commission")
+
