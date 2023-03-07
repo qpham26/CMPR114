662d600ac0cbc05ded6b612b82779a233405f69f
qpham26 <qpham26@gmail.com>
2023-03-06 19:07:42 -0800

    Merge remote-tracking branch 'origin/main'

---
+++ Week 4/Quiz 1/Quiz 1.3.py
@@ -0,0 +1,7 @@
+#Program takes first and last name with age and output the result
+
+first = input("Enter your first name: ")
+last = input("Enter your last name: ")
+age = str(input("Enter your age: "))
+
+print(f'{first} {last}, {age}')
