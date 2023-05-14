fbf0575c9fc05f0f71525efe7b7d04faa5da8444
qpham26 <qpham26@gmail.com>
2023-05-13 22:12:29 -0700

    Merge remote-tracking branch 'origin/main'

--- Week 12/HW #14/HW 14.4.py
+++ Week 12/HW #14/HW 14.4.py
@@ -17,24 +17,30 @@ class MyGUI:
         self.cb_var1 = tkinter. IntVar()
         self.cb_var2 = tkinter. IntVar()
         self.cb_var3 = tkinter. IntVar()
-
+        self.cb_var4 = tkinter.IntVar()
+        self.cb_var5 = tkinter.IntVar()
 
         # Set the intVar objects to 0.
         self.cb_var1.set(0)
         self.cb_var2.set(0)
         self.cb_var3.set(0)
-
+        self.cb_var4.set(0)
+        self.cb_var5.set(0)
 
 
         # Create the Checkbutton widgets in the top_frame.
         self.cb1 = tkinter.Checkbutton(self.top_frame, text='Option 1', variable=self.cb_var1)
         self.cb2 = tkinter.Checkbutton(self.top_frame, text='Option 2', variable=self.cb_var2)
         self.cb3 = tkinter.Checkbutton(self.top_frame, text='Option 3', variable=self.cb_var3)
+        self.cb4 = tkinter.Checkbutton(self.top_frame, text='Option 4', variable=self.cb_var4)
+        self.cb5 = tkinter.Checkbutton(self.top_frame, text='Option 5', variable=self.cb_var5)
 
         # Pack the Checkbuttons.
         self.cb1.pack()
         self.cb2.pack()
         self.cb3.pack()
+        self.cb4.pack()
+        self.cb5.pack()
 
         # Create an OK button and a Quit button.
         self.ok_button = tkinter.Button(self.bottom_frame, text='OK', command=self.show_choice)
@@ -67,6 +73,10 @@ class MyGUI:
             self.message = self.message + '2\n'
         if self.cb_var3.get() == 1:
             self.message = self.message + '3\n'
+        if self.cb_var4.get() == 1:
+            self.message = self.message + '4\n'
+        if self.cb_var5.get() == 1:
+            self.message = self.message + '5\n'
 
         # Display the message in an info dialog box.
         tkinter.messagebox.showinfo('Selection', self.message)
